f50df7b50d967dae46ada710f79d3c91c5fcc5fd
rvvictor <victorm1217@yahoo.com>
2023-11-06 23:06:15 -0600

    ya

--- web/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("burger-icon").addEventListener("click", function () {
-  document.getElementById("sidebar").classList.toggle("active-sidebar");
-  document.getElementById("burger-icon").classList.toggle("active"); // Agregamos o quitamos la clase 'active'
-});
