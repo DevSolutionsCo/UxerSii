d2645a363345de46de73acf831b2dedd91202b3b
CrXTrhx <cristopherdmolina@gmail.com>
2024-02-02 02:33:28 -0600

    resolvi

--- uxersii/src/components/Popups/Popup.tsx
+++ uxersii/src/components/Popups/Popup.tsx
@@ -1,47 +1,32 @@
-import clsx from "clsx";
-import { ReactNode, useEffect, useState } from "react";
 import "./styleepopup.css";
+import { ReactNode } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   children?: ReactNode;
   titulo?: ReactNode;
   texbtn?: ReactNode;
-  isOpen?: boolean;
-  onClose: () => void;
 }
 
 function Popup(props: Props) {
-  const [modalAbierto, setModalAbierto] = useState(!!props.isOpen);
+  const [modalAbierto, setModalAbierto] = useState(false);
 
   useEffect(() => {
-<<<<<<< HEAD
     console.log(`El modal está ${modalAbierto ? "abierto" : "cerrado"}`);
   }, [modalAbierto]);
 
   const abrirModal = () => {
      setModalAbierto(true);
    };
-=======
-    setModalAbierto(!!props.isOpen);
-  }, [props.isOpen]);
->>>>>>> ffd7029b1e7744dd51fd9c13131a7a1d3b91f061
 
   const cerrarModal = () => {
     setModalAbierto(false);
-    props.onClose();
   };
-
   return (
-<<<<<<< HEAD
     <>
       <button className="btn-ope px-8 py-5 relative rounded-xl font-bold text-black w-full" onClick={abrirModal}>Abrir Modal</button>
       {modalAbierto && (
         <div className="popup" id="abrir-pop">
-=======
-    <div>
-      {modalAbierto && (
-        <div className={clsx("popup", { "abrir-pop": modalAbierto })}>
->>>>>>> ffd7029b1e7744dd51fd9c13131a7a1d3b91f061
           <div className="nav-popup font-bold">
             <div className="text-navbar-popup">
               <p className="titulo-popup">{props.titulo}</p>
@@ -52,11 +37,7 @@ function Popup(props: Props) {
               </button>
             </div>
           </div>
-<<<<<<< HEAD
           {props.children}
-=======
-          <div className="p-6">{props.children}</div>
->>>>>>> ffd7029b1e7744dd51fd9c13131a7a1d3b91f061
           <div>
             <button className="btn-seguir px-6 py-3 relative rounded-xl font-bold text-black w-full">
               {props.texbtn}
@@ -64,11 +45,7 @@ function Popup(props: Props) {
           </div>
         </div>
       )}
-<<<<<<< HEAD
     </>
-=======
-    </div>
->>>>>>> ffd7029b1e7744dd51fd9c13131a7a1d3b91f061
   );
 }
 
