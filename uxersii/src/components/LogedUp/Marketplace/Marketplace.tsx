46f22b9efff8a654a6e70be504edd9f4b26cbb09
CrXTrhx <cristopherdmolina@gmail.com>
2024-02-11 20:07:41 -0600

    puedes chambear javi

--- uxersii/src/components/LogedUp/Marketplace/Marketplace.tsx
+++ uxersii/src/components/LogedUp/Marketplace/Marketplace.tsx
@@ -1,27 +1,308 @@
-import "./stylemarcketplace.css"
-
-
+import "./stylemarcketplace.css";
 
 function Marketplace() {
   return (
-    <section>
-      <h2>Productos</h2>
-      <div className="pro-conten shadow-xl shadow-black">
-        <div className="prod">
-          <img
-            src="https://statics-cuidateplus.marca.com/cms/platanos_0.jpg"
-            alt="Producto"
-            className="w-44 h-60 img-prod"
-          />
-          <div className="desc">
-            <span>Tiempo de caducidad</span>
-            <h5>Platanito</h5>
-            <span>120kg</span>
-            <h4>$120</h4>
-          </div>
+    <>
+      <nav className="nav-market flex flex-row justify-between items-center px-4 sm:px-10 sm:flex-row bg-slate-50 dark:bg-zinc-800">
+        <div>
+          <select name="puntos" id="" className="select-car">
+            <option value="valor1" className="first-option options">
+              Av. Nacional S/n
+            </option>
+            <option value="valor1" className="seconds-option options">
+              Av. Nacional S/n
+            </option>
+            <option value="valor1" className="seconds-option options">
+              Av. Nacional S/n
+            </option>
+            <option value="valor1" className="seconds-option options">
+              Av. Nacional S/n
+            </option>
+          </select>
         </div>
-      </div>
-    </section>
+        <div className="flex flex-row justify-center carrito ">
+          <button>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/18/18399.png"
+              alt="Mi carrito"
+              className="img-carrito"
+            />
+          </button>
+          <p className="w-car font-bold">Carrito</p>
+        </div>
+      </nav>
+
+      <section className=" flex flex-row show-prod">
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+      </section>
+      <section className=" flex flex-row show-prod">
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+      </section>
+      <section className=" flex flex-row show-prod">
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+      </section>
+      <section className=" flex flex-row show-prod">
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div className="pro-conten shadow-md shadow-black">
+          <div className="prod">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/009/597/420/original/banana-fruit-illustration-cartoon-png.png"
+              alt="Producto"
+              className="w-44  img-prod"
+            />
+            <div className="desc">
+              <span className="font-extralight time-cad">06/05/04</span>
+              <h5 className="font-bold name-prod">Platanitos</h5>
+              <span className="font-medium">120kg</span>
+              <div className="flex flex-row justify-between items-center">
+              <h4 className="price font-semibold">$120</h4>
+              <div>
+              <button className="font-extrabold btn-agregar">+</button>
+              <button className="font-bold btn-rest">-</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+      </section>
+     
+
+    </>
   );
 }
 
