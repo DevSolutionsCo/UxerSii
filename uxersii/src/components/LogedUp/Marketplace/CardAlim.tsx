a039d3180536cd9b46ba7cbea59ca11ea12d5880
Seebaastiaan <sebasjara1503@outlook.com>
2024-02-26 12:53:25 -0600

    detallitos

--- uxersii/src/components/LogedUp/Marketplace/CardAlim.tsx
+++ uxersii/src/components/LogedUp/Marketplace/CardAlim.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import BotonLogin from '../../Signed out/login/BotonLogin';
 interface props{
 img:string;
@@ -7,21 +7,21 @@ title:string;
 peso:string;
 precio:string;
 } 
-
 function CardAlim(props: props) {
     return (
-      <div className="my-4 p-2 border border-black rounded-md">
+      <div className="my-4 p-2 border border-black rounded-md flex flex-col h-full">
         <img src={props.img} className="w-full h-24 object-cover rounded-t-md" alt="Imagen del producto" />
-        <div className="p-2">
-          <h2 className="text-sm font-semibold">{props.fecha}</h2>
-          <h2 className="text-base font-bold">{props.title}</h2>
-          <h2 className="text-sm">Cantidad: <span className="font-bold">{props.peso}</span></h2>
-          <h2 className="text-sm">{props.precio}</h2>
-          <BotonLogin className="mt-2 bg-[#C3DDFF] border-2 px-4 py-2 rounded-md font-bold text-black w-full">Agregar</BotonLogin>
+        <div className="flex-grow flex flex-col justify-between p-2">
+          <div className='py-7'>
+            <h2 className="text-sm font-semibold">{props.fecha}</h2>
+            <h2 className="text-base font-bold">{props.title}</h2>
+            <h2 className="text-sm">Cantidad: <span className="font-bold">{props.peso}</span></h2>
+            <h2 className="text-sm">{props.precio}</h2>
+          </div>
+          <BotonLogin className="mt-2 bg-[#C3DDFF] border-2 px-4 py-2 rounded-md font-bold text-black self-end w-full">Agregar</BotonLogin>
         </div>
       </div>
     );
   }
   
   export default CardAlim;
-  
