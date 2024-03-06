6082f6e65fed7a2cc42cb5e89e264defd5c0b449
Seebaastiaan <sebasjara1503@outlook.com>
2024-03-05 22:35:31 -0600

    RecuperedatosporURL

--- uxersii/src/components/LogedUp/Marketplace/CardAlim.tsx
+++ uxersii/src/components/LogedUp/Marketplace/CardAlim.tsx
@@ -1,5 +1,6 @@
 // CardAlim.tsx
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import BotonLogin from "../../Signed out/login/BotonLogin";
 
 interface Props {
@@ -20,28 +21,33 @@ function CardAlim(props: Props) {
   };
 
   return (
-    <div className="my-4 p-2 border shadow-black/50 shadow-sm rounded-md flex flex-col h-full">
-      <img
-        src={props.img}
-        className="w-full h-24 object-cover rounded-t-md"
-        alt="Imagen del producto"
-      />
-      <div className="flex-grow flex flex-col justify-between p-2">
-        <div className="py-7">
-          <h2 className="text-sm font-semibold">{props.fecha}</h2>
-          <h2 className="text-base font-bold">{props.title}</h2>
-          <h2 className="text-sm">
-            Cantidad: <span className="font-bold">{props.peso}</span>
-          </h2>
-          <h2 className="text-sm">{props.precio}</h2>
+    <div className="my-4 p-2 border shadow-black/50 shadow-sm rounded-md flex flex-col h-full cursor-pointer">
+      <Link
+        to={`item?img=${props.img}&fecha=${props.fecha}&title=${props.title}&peso=${props.peso}&precio=${props.precio}`}
+      >
+        {" "}
+        <img
+          src={props.img}
+          className="w-full h-24 object-cover rounded-t-md"
+          alt="Imagen del producto"
+        />
+        <div className="flex-grow flex flex-col justify-between p-2">
+          <div className="py-7">
+            <h2 className="text-sm font-semibold">{props.fecha}</h2>
+            <h2 className="text-base font-bold">{props.title}</h2>
+            <h2 className="text-sm">
+              Cantidad: <span className="font-bold">{props.peso}</span>
+            </h2>
+            <h2 className="text-sm">{props.precio}</h2>
+          </div>
         </div>
-        <BotonLogin
-          className="mt-2 bg-[#C3DDFF] border-2 px-4 py-2 rounded-md font-bold text-black self-end w-full"
-          onClick={handleClickAgregar}
-        >
-          Agregar
-        </BotonLogin>
-      </div>
+      </Link>
+      <BotonLogin
+        className="mt-2 bg-[#C3DDFF] border-2 px-4 py-2 rounded-md font-bold text-black self-end w-full"
+        onClick={handleClickAgregar}
+      >
+        Agregar
+      </BotonLogin>
     </div>
   );
 }
