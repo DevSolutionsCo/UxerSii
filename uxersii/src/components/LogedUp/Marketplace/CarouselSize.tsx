0128090d6b982a6a6faec357ac0a8299481783d4
Crhtxx <si@si.com>
2024-06-04 19:53:19 -0600

    carrousel responsive

--- uxersii/src/components/LogedUp/Marketplace/CarouselSize.tsx
+++ uxersii/src/components/LogedUp/Marketplace/CarouselSize.tsx
@@ -29,24 +29,27 @@ export function CarouselSize(props: Props) {
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
             <div className="p-1">
               <Card className="h-[350px] cursor-pointer">
-                <div className="flex p-4 flex-col">
+                <div className="flex flex-col p-4 h-full">
                   <div className="flex justify-center items-center pb-4">
-                  <img src={product.imagen} className="w-32 h-32 pb-1" />
+                    <img
+                      src={product.imagen}
+                      className="w-32 h-32 pb-1"
+                      alt="Product Image"
+                    />
                   </div>
-                  <div className="flex flex-col justify-start items-start">
-                    <p>
+                  <div className="flex flex-col justify-start items-start flex-grow">
+                    <p className="w-full">
                       {product.fecha_cad}
-                      <p className="text-xl font-bold">
-                        {product.nomb_alim}
-                      </p>
-                      <span>Cantidad:{product.cantidad}</span>
+                      <p className="text-xl font-bold">{product.nomb_alim}</p>
+                      <span>Cantidad: {product.cantidad}</span>
                     </p>
                   </div>
-                  <div className="flex justify-between w-full mt-[45px] items-center">
-                    <span>
-                      <span className="text-lg ml-16 font-bold ">${product.costo}<span className="text-sm">MXN</span> </span>
+                  <div className="flex justify-between items-center mt-auto">
+                    <span className="text-lg font-bold">
+                      ${product.costo}
+                      <span className="text-sm">MXN</span>
                     </span>
-                    <BotonLogin className="mt-2 bg-[#F03849] border-2 px-4 py-2 rounded-md font-bold text-white self-end w-1/3">
+                    <BotonLogin className="bg-[#F03849] border-2 px-4 py-2 rounded-md font-bold text-white flex items-center justify-center w-auto sm:w-1/3 mt-2">
                       <AddShoppingCartIcon />
                     </BotonLogin>
                   </div>
