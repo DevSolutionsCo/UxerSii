561c435c92fc83fe17f3b6b63296490e0f6031b4
JaAv <javisrey26@gmail.com>
2024-06-04 20:05:18 -0600

    Sober - KiLLOWEEN

--- uxersii/src/components/LogedUp/Marketplace/CarouselSize.tsx
+++ uxersii/src/components/LogedUp/Marketplace/CarouselSize.tsx
@@ -107,6 +107,11 @@ export function CarouselSize(props: Props) {
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
+      {showAlert && (
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-4 py-2 rounded-md">
+          Producto agregado al carrito
+        </div>
+      )}
     </Carousel>
   );
 }
